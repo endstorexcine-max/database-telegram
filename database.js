2a86ddc11dde36c59886c1ccdbbcd4c80dcf9723
endstorexcine-max <endstorexcine@gmail.com>
2026-02-21 20:07:06 +0700

    database.js

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 {
   "tokens": [ 
     "8401166943:AAH5JLRV7ZRTnKpx7jeuDDyuicOFw6gqzi0",
-    "8316708496:AAEMLwWbZk_xiQlEvR9P2nUVBstnBOv9CNw"
+    "8316708496:AAEMLwWbZk_xiQlEvR9P2nUVBstnBOv9CNw",
+    "7966375454:AAGhkknJSYXrGArfLcwSa8Pnkc772nmyNWc"
     ]
 }
